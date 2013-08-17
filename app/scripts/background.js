bf6ce46e2fe8d391322e7aa984080ca1065b911b
rowan <rowanu@gmail.com>
2013-08-17 14:06:25 -0400

    include google's GAPI client.js. make popup display events retrieved by background

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,32 +1,48 @@
+/*jslint indent: 2, browser: true, vars: true */
+/*globals chrome: false, gapi: false */
 'use strict';
 
+// Code from:
+// https://code.google.com/p/google-api-javascript-client/source/browse/samples/authSample.html
 chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+  var clientId = '161424625207';
+  var apiKey = 'AIzaSyBtDGNHd29wHMOHk9Mw-HWu_KsufPxnNTw';
+  var scopes = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly';
 
-  var oauth = ChromeExOAuth.initBackgroundPage({
-    'request_url' : 'https://www.google.com/accounts/OAuthGetRequestToken',
-    'authorize_url' : 'https://www.google.com/accounts/OAuthAuthorizeToken',
-    'access_url' : 'https://www.google.com/accounts/OAuthGetAccessToken',
-    'consumer_key' : 'anonymous',
-    'consumer_secret' : 'anonymous',
-    'scope' : 'https://www.googleapis.com/auth/calendar',
-    'app_name' : 'Chrome Calendar'
-  });
+  // Load the API and make an API call.  Display the results on the screen.
+  var makeApiCall = function () {
+    gapi.client.load('calendar', 'v3', function () {
+      var request = gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'maxResults': 10
+      });
 
-  function callback(resp, xhr) {
-    console.log(resp);
+      request.execute(function (response) {
+        console.log(response);
+
+        window.eventies = response.items;
+      });
+    });
   };
 
-  // TODO: move anon function out
-  oauth.authorize(function() {
-    console.log("OHAI")
-    var url = 'https://www.googleapis.com/calendar/v3/users/me/calendarList';
-    var request = {
-      'method': 'GET',
-      'parameters': {'alt': 'json'}
-    };
+  var handleAuthResult = function (authResult) {
+    if (authResult && !authResult.error) {
+      console.log("Authenticated");
+      makeApiCall();
+    } else {
+      console.log("Uh oh...");
+    }
+  };
 
-    // Send: GET https://docs.google.com/feeds/default/private/full?alt=json
-    oauth.sendSignedRequest(url, callback, request);
-  });
+  var checkAuth = function () {
+    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+  };
+
+  // Use a button to handle authentication the first time.
+  var handleClientLoad = function () {
+    gapi.client.setApiKey(apiKey);
+    window.setTimeout(checkAuth, 1);
+  };
+
+  handleClientLoad();
 });
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,26 @@
+/*jslint indent: 2, vars: true, browser: true */
+/*globals chrome: false */
 'use strict';
+
+// TODO: Use moment.js to format dates nicely
+
+document.addEventListener('DOMContentLoaded', function () {
+  var i;
+  var backgroundPage = chrome.extension.getBackgroundPage();
+  var events = backgroundPage.eventies.map(function (i) { return i.summary + ": " + i.start.dateTime + " to " + i.end.dateTime; });
+  var ul = document.createElement("ul");
+  for (i = 0; i < events.length; i += 1) {
+    var li = document.createElement("li");
+    li.innerText = events[i];
+    ul.appendChild(li);
+  }
+  document.body.appendChild(ul);
+
+    // chrome.extension.onMessage.addListener(function (response) {
+    //   console.log("OHAI client");
+    //   // console.log(response);
+    //   // var div = document.createElement("div");
+    //   // var message = document.createTextNode(response.items.join("<br />\n"));
+    //   // console.log(message);
+    //   // div.appendChild(message);
+});
