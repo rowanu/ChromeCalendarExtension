c620d82077934a6a6407d2689d3cbf628a11d2cc
rowan <rowanu@gmail.com>
2013-08-23 11:37:43 -0400

    include underscore and momentjs via bower (closes #4).

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,5 @@
-/*jslint indent: 2, browser: true, vars: true */
-/*globals chrome: false, gapi: false, moment: false */
+/*jslint indent: 2, browser: true, vars: true, nomen: true */
+/*globals chrome: false, gapi: false, moment: false, _: false */
 "use strict";
 (function () {
   var clientId = "750882903724";
@@ -9,15 +9,22 @@
   var getEvents = function () {
     console.log("Getting events");
     gapi.client.load("calendar", "v3", function () {
+      // TODO: All-day events have NO TIMEZONE - ignore?
       // Get today's events
       var request = gapi.client.calendar.events.list({
-        "calendarId": "primary",
-        "timeMin": moment().toDate(),
-        "timeMax": moment().endOf("day").toDate()
+        calendarId: "primary",
+        singleEvents: true,
+        showDeleted: false,
+        timeMin: moment().toDate(),
+        timeMax: moment().endOf("day").toDate()
       });
       request.execute(function (response) {
-        var i, numberOfEvents = response.items.length;
+        var numberOfEvents = response.hasOwnProperty("items") ? response.items.length : "0";
         console.log("Events updated (" + numberOfEvents + " received)");
+        _.each(response.items, function (e, i) {
+          console.log(i + ": " + e.summary + " (" + e.status + ")");
+          console.log(e);
+        });
         // Update badge with number of events left today
         chrome.browserAction.setBadgeText({text: numberOfEvents.toString()});
       });
